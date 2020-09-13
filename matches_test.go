976075b7b081ea107a00639bb4a11cdf379a8349
Philipp Naumann <phinau@gmail.com>
2020-09-13 20:21:47 +0200

    Test cases

--- matches_test.go
+++ matches_test.go
@@ -35,12 +35,19 @@ func TestMatches(t *testing.T) {
 	ml("invalid 2", "192.168.1.1", "%host% extra", false)
 	ml("invalid 3", "1200:0000:AB00:1234:O000:2552:7777:1313", "%host%", false)
 
-	em("valid 1", "0.0.0.0", false)
-	em("valid 2", "11.0.0.0", false)
-	em("valid 3", "129.56.0.0", false)
-	em("valid 4", "243.8.45.0", false)
-	em("valid 5", "192.168.172.14", false)
-	em("valid 6", "1200:0000:AB00:1234:0000:2552:7777:1313", true)
-	em("valid 7", "21DA:D3:0:2F3B:2AA:FF:FE28:9C5A", true)
-	em("valid 8", "::1", true)
+	if m := ml("valid 1.1", "prefix 192.168.1.1", "prefix.*%host%", true); m.host != "192.168.1.1" {
+		t.Errorf(`expected host "192.168.1.1", got "%s"`, m.host)
+	}
+	if m := ml("valid 1.2", "192.168.1.1 suffix", "%host%.*suffix", true); m.host != "192.168.1.1" {
+		t.Errorf(`expected host "192.168.1.1", got "%s"`, m.host)
+	}
+
+	em("valid 2.1", "0.0.0.0", false)
+	em("valid 2.2", "11.0.0.0", false)
+	em("valid 2.3", "129.56.0.0", false)
+	em("valid 2.4", "243.8.45.0", false)
+	em("valid 2.5", "192.168.172.14", false)
+	em("valid 2.6", "1200:0000:AB00:1234:0000:2552:7777:1313", true)
+	em("valid 2.7", "21DA:D3:0:2F3B:2AA:FF:FE28:9C5A", true)
+	em("valid 2.8", "::1", true)
 }
