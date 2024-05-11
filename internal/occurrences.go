8d727036ce0301df4e5127106e555563534ea338
Philipp Naumann <phinau@gmail.com>
2024-05-11 08:22:03 +0200

    Refactor receiver name

--- internal/occurrences.go
+++ internal/occurrences.go
@@ -13,26 +13,26 @@ type occurrences struct {
 	count    int
 }
 
-func (r *occurrences) add(ip net.IP) bool {
+func (o *occurrences) add(ip net.IP) bool {
 	ips := ip.String()
 	t := time.Now()
 
-	log.Debug().IPAddr("ip", ip).Int("length", len(r.registry[ips])).Msg("updating occurrences")
+	log.Debug().IPAddr("ip", ip).Int("length", len(o.registry[ips])).Msg("updating occurrences")
 
-	if _, f := r.registry[ips]; !f {
-		r.registry[ips] = []time.Time{t}
+	if _, f := o.registry[ips]; !f {
+		o.registry[ips] = []time.Time{t}
 		return false
 	}
 
-	r.registry[ips] = append(r.registry[ips], t)
-	if len(r.registry[ips]) > r.count {
-		r.registry[ips] = r.registry[ips][1:]
+	o.registry[ips] = append(o.registry[ips], t)
+	if len(o.registry[ips]) > o.count {
+		o.registry[ips] = o.registry[ips][1:]
 	}
 
-	if len(r.registry[ips]) == r.count {
-		d := r.registry[ips][r.count-1].Sub(r.registry[ips][0])
-		if d <= r.interval {
-			delete(r.registry, ips)
+	if len(o.registry[ips]) == o.count {
+		d := o.registry[ips][o.count-1].Sub(o.registry[ips][0])
+		if d <= o.interval {
+			delete(o.registry, ips)
 			return true
 		}
 	}
